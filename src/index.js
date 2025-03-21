ed99c9d605bdc5eba4d135262be7f38fc022730e
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-22 00:21:18 +0300

    taskliste complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myToDosList = [];
+
+  const myTaskForm = document.querySelector("#create-task-form");
+  const unorderedListsElement = document.querySelector("#tasks");
+
+  myTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const firstTaskInput = document.getElementById("new-task-description");
+    const taskText = firstTaskInput.value.trim();
+
+    if (taskText !== "") {
+      myToDosList.push(taskText);
+
+      renderMyToDoList(myToDosList);
+
+      myTaskForm.reset();
+    }
+  });
+
+  function renderMyToDoList(mytodos) {
+    unorderedListsElement.innerHTML = "";
+
+    mytodos.forEach((mytodos, index) => {
+      const li = document.createElement("li");
+      li.textContent = mytodos;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "X";
+      deleteButton.addEventListener("click", () => {
+        myToDosList.splice(index, 1);
+
+        renderMyToDoList(myToDosList);
+      });
+
+      li.appendChild(deleteButton);
+
+      unorderedListsElement.appendChild(li);
+    });
+  }
 });
